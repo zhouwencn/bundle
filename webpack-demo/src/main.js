97b58d86f06e8db4d8d6e061f214600d7a113ef7
New Author Name <739856334zhou@gmail.com>
2022-06-08 19:44:22 +0800

    各类css的loader

--- webpack-demo/src/main.js
+++ webpack-demo/src/main.js
@@ -1,3 +1,7 @@
-import { add } from './test'
+import { add } from './js/test'
+import './css/indexOne.css'
+import './css/indexTwo.less'
+import './css/indexThree.scss'
+import './css/indexFour.styl'
 const result = add(6, 6)
 console.log(result)
