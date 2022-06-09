f14ac76acc55081d2e8668e8b8a3544e0015d84f
New Author Name <739856334zhou@gmail.com>
2022-06-09 12:58:00 +0800

    提取css成单独的问题，在通过link标签引入

--- webpack-demo/config/webpack.dev.js
+++ webpack-demo/config/webpack.dev.js
@@ -1,6 +1,7 @@
 const path = require('path')
 const ESLintPlugin = require('eslint-webpack-plugin')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
 module.exports = {
   entry: './src/main.js',
   output: {
@@ -19,18 +20,18 @@ module.exports = {
         // 执行顺序，从右到左
         // css-loader将css资源编译成commonjs的模块到js中
         // style-loader将js中的css通过创建style标签添加html文件中生效
-        use: ['style-loader', 'css-loader'],
+        use: [MiniCssExtractPlugin.loader, 'css-loader'],
       },
       // compiles Less to CSS
       {
         test: /\.less$/,
-        use: ['style-loader', 'css-loader', 'less-loader'],
+        use: [MiniCssExtractPlugin.loader, 'css-loader', 'less-loader'],
       },
       {
         test: /\.s[ac]ss$/,
         use: [
           // Creates `style` nodes from JS strings
-          'style-loader',
+          MiniCssExtractPlugin.loader,
           // Translates CSS into CommonJS
           'css-loader',
           // Compiles Sass to CSS
@@ -42,7 +43,7 @@ module.exports = {
         // 换一种写法
         use: [
           {
-            loader: 'style-loader', // creates style nodes from JS strings
+            loader: MiniCssExtractPlugin.loader, // creates style nodes from JS strings
           },
           {
             loader: 'css-loader', // translates CSS into CommonJS
@@ -106,6 +107,9 @@ module.exports = {
     }),
     new HtmlWebpackPlugin({
       template: path.resolve(__dirname, '../public/index.html'),
+    }),
+    new MiniCssExtractPlugin({
+      filename: 'static/css/main.css',
     }),
   ],
   //...
